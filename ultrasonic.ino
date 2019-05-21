a531e32c815cdec972a383343f0589138a6f64d0
NavyaMirthipati <50670761+NavyaMirthipati@users.noreply.github.com>
2019-05-21 12:05:25 +0530

    Add files via upload

--- ultrasonic.ino
+++ ultrasonic.ino
@@ -22,13 +22,5 @@ void loop() {
  distance=duration*0.034/2;
  Serial.print("Distance:");
  Serial.println(distance);
- if(distance > 60)
- {
-  digitalWrite(2,HIGH);
- }
- else
- {
-  digitalWrite(2,LOW);
- }
- 
+
 }
