748544179c6ac727a8ca521bf9c5a2d6361032be
NavyaMirthipati <50670761+NavyaMirthipati@users.noreply.github.com>
2019-05-22 00:31:40 -0400

    Add files via upload

---
+++ ultraservo.ino
@@ -0,0 +1,42 @@
+
+#include <Servo.h>
+Servo myservo;
+const int trigPin = 4;
+const int echoPin = 5;
+int pos = 0;
+
+long duration;
+int distance;
+
+void setup() {
+  pinMode(2,OUTPUT);
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin,INPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(100);
+  digitalWrite(trigPin,LOW);
+  duration = pulseIn(echoPin,HIGH);
+  distance = duration * 0.034/2;
+  Serial.print("Distance:");
+  Serial.println(distance);
+  for(pos = 0;pos <= 180;pos +=1)
+  {
+    myservo.write(pos);
+    if(pos==90)
+    delay(200);
+    delay(15);
+  }
+  for(pos = 180;pos >= 0;pos -=1)
+  {
+    myservo.write(pos);
+    if(pos==90)
+    delay(200);
+    delay(15);
+  }
+}
